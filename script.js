8c4d8e1fd1158524b246bdf83fcf1fc441322066
Boris <savelyevbb@mail.ru>
2023-02-06 14:43:43 +0500

    add adaptive elements

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-// Слайдер
-
+// Константы и переменные
 const slider = document.querySelector('.slider');
 const sliderContainer = slider.querySelector('.slider__container');
 const sliderItems = sliderContainer.querySelectorAll('.slider__item');
 const prevSlide = slider.querySelector('#slider-prev');
 const nextSlide = slider.querySelector('#slider-next');
+const bikesSelect = document.querySelectorAll('.bikes__links');
+const bikesPuprose = document.querySelectorAll('.bikes__purpose');
+const switcher = document.querySelectorAll('.theme-switcher');
+const formInput = document.querySelector('.form__input');
+const formButton = document.querySelector('.form__button')
+const form = document.querySelector('.form');
+const selector = document.querySelector('.bikes__selector')
 
 let offset = 0;
 
+// Слайдер
 showSlides(prevSlide, nextSlide);
 checkButton();
 
@@ -30,45 +37,36 @@ function showSlides(prev, next) {
   });
 }
 // Функция переключения слайдов 
-
-const bikesSelector = document.querySelectorAll('.bikes__selector');
-const bikesPuprose = document.querySelectorAll('.bikes__purpose');
-
-function switcherSelector(button, selector) {
+function switcherSelector(button, select) {
   for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', function () {
-      for (let j = 0; j < selector.length; j++) {
-        selector[j].classList.remove('bikes__selector_opened');
+      for (let j = 0; j < select.length; j++) {
+        select[j].classList.remove('bikes__links_opened');
         button[j].classList.remove('bikes__purpose_active');
       }
       button[i].classList.add('bikes__purpose_active');
-      selector[i].classList.add('bikes__selector_opened');
+      select[i].classList.add('bikes__links_opened');
     });
   }
 }
 
-switcherSelector(bikesPuprose, bikesSelector);
+switcherSelector(bikesPuprose, bikesSelect);
 
 // Свитчер темы
-
-const switcher = document.querySelector('.theme-switcher');
-
-switcher.querySelector('.theme-switcher__container').addEventListener('click', () => {
-  switcher.classList.toggle('theme-switcher_theme_dark');
-  switcher.querySelector('.theme-switcher__toggler').classList.toggle('theme-switcher__toggler_active');
-  document.querySelector('.page').classList.toggle('page_theme_dark');
-  document.querySelectorAll('.slider__button').forEach((button) => {
-    button.classList.toggle('slider__button_theme_dark')
+switcher.forEach(item => {
+    item.querySelector('.theme-switcher__container').addEventListener('click', () => {
+    item.classList.toggle('theme-switcher_theme_dark');
+    item.querySelector('.theme-switcher__toggler').classList.toggle('theme-switcher__toggler_active');
+    document.querySelector('.page').classList.toggle('page_theme_dark');
+    document.querySelectorAll('.slider__button').forEach(button => {
+      button.classList.toggle('slider__button_theme_dark')
+    });
+    document.querySelector('.footer').classList.toggle('footer_theme_dark');
+    document.querySelector('.header__menu-button').classList.toggle('header__menu-button_theme_dark')
   });
-  document.querySelector('.footer').classList.toggle('footer_theme_dark')
-});
-
+}) 
 
 // Форма для почты
-
-const formInput = document.querySelector('.form__input');
-const formButton = document.querySelector('.form__button');
-
 function checkInput(input, button) {
   input.addEventListener('input', () => {
     if (input.value.includes('@') && input.value.length > 3) {
@@ -82,7 +80,36 @@ function checkInput(input, button) {
 checkInput(formInput, formButton);
 
 // Функция вывода "Круто"
-
-formButton.addEventListener('click', () => {
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
   formInput.value = 'Круто!'
 })
+
+// Функция показа слайда по опции селектора
+function selectItem(selector, select) {
+  selector.addEventListener('change', () => {
+    select.forEach(item => {
+      item.classList.remove('bikes__links_opened');
+      for (let i=0; i < selector.length; i++) {
+        if(selector.value === select[i].id) {
+          select[i].classList.add('bikes__links_opened')
+        }
+      }
+    });
+  })
+}
+
+selectItem(selector, bikesSelect)
+
+// Функция вызова меню
+document.querySelector('.header__menu-button').addEventListener('click', () => {
+  document.querySelector('.header__menu-button').classList.toggle('header__menu-button_close')
+  document.querySelector('.burger-menu').classList.toggle('burger-menu_active')
+})
+
+document.querySelectorAll('.menu__link').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelector('.burger-menu').classList.remove('burger-menu_active');
+    document.querySelector('.header__menu-button').classList.remove('header__menu-button_close')
+  })
+});
